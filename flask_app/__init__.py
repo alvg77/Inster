6b0111b3ab030974033e5a0391be55b24545e2a4
Aleko Georgiev <61247544+po1nt3r@users.noreply.github.com>
2021-06-05 00:50:27 +0300

    aaaaaaaaaaaaaaaaaaaaaaaa

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -13,7 +13,7 @@ whooshee.reindex()
 app.config['SECRET_KEY'] = 'c78119002dc96180e56f64c789a7d732b74c83dd23d63be147'
 bcrypt = Bcrypt()
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fsjvniytpfyucz:767f0e79306a31257018002b6fed9ada05f578e00c6e68f51e6e746d55cdfc2f@ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d8mdtd68aeqmm4"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://fsjvniytpfyucz:767f0e79306a31257018002b6fed9ada05f578e00c6e68f51e6e746d55cdfc2f@ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d8mdtd68aeqmm4"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
